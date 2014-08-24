89bd1db579815f6a443da715143803fa780a8dd3
Eugenio Paolantonio (g7) <me@medesimo.eu>
2014-08-24 13:14:50 +0200

    Relicense to LGPL2.1, updated headers to match it.

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
-# keeptalking setup (using distutils)
-# Copyright (C) 2012 Eugenio "g7" Paolantonio. All rights reserved.
-# Work released under the GNU GPL license, version 3.
+# -*- coding: utf-8 -*-
+#
+# keeptalking2 - library to interface with internationalization features
+# Copyright (C) 2012-2014  Eugenio "g7" Paolantonio
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Authors:
+#    Eugenio "g7" Paolantonio <me@medesimo.eu>
+#
 
 from distutils.core import setup
 
 setup(name='keeptalking2',
 	version='6.20.0',
-	description='Language/Keyboard/Timezone libraries',
+	description='Library to interface with internationalization features',
 	author='Eugenio Paolantonio',
 	author_email='me@medesimo.eu',
 	url='http://github.com/semplice/keeptalking2',
@@ -16,5 +35,5 @@ setup(name='keeptalking2',
 		"keeptalking2",
 		"keeptalking2.core",
       ],
-	requires=['threading', 'time', 'locale', 'fileinput', 'os', 'sys', 'shutil'],
+	requires=['gi.repository.Gio', 'time', 'fileinput', 'os', 'sys', 'shutil'],
 )
